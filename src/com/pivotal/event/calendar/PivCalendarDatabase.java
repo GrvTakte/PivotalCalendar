db6033d78277d646422481f72d8703c7167b3a71
GrvTakte <gauravtakte@gmail.com>
2017-04-12 11:51:14 +0530

    adding Event handling UI to the application

---
+++ src/com/pivotal/event/calendar/PivCalendarDatabase.java
@@ -0,0 +1,15 @@
+
+package com.pivotal.event.calendar;
+
+import com.codename1.ui.Calendar;
+
+/**
+ *
+ * @author gaurav
+ */
+public class PivCalendarDatabase {
+    
+   
+    
+    
+}
--- src/com/pivotal/event/calendar/PivDisplayCalendar.java
+++ src/com/pivotal/event/calendar/PivDisplayCalendar.java
@@ -1,15 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.pivotal.event.calendar;
 
-import com.codename1.components.SpanLabel;
 import com.codename1.io.Log;
 import com.codename1.ui.Button;
 import com.codename1.ui.ComboBox;
-import com.codename1.ui.Component;
 import com.codename1.ui.Container;
 import com.codename1.ui.Display;
 import com.codename1.ui.Label;
@@ -17,6 +10,7 @@ import com.codename1.ui.events.ActionEvent;
 import com.codename1.ui.events.ActionListener;
 import com.codename1.ui.layouts.BorderLayout;
 import com.codename1.ui.layouts.BoxLayout;
+import com.codename1.ui.layouts.FlowLayout;
 import com.codename1.ui.layouts.GridLayout;
 import com.codename1.ui.plaf.UIManager;
 import com.codename1.ui.spinner.Picker;
@@ -29,10 +23,13 @@ import java.util.ArrayList;
 public class PivDisplayCalendar extends Container {
     
     
-    int length =37;
+    int length =31;
      private ComboBox year;
      private static final String[] DAYS = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
      private static final String[] LABELS = {"Su", "M", "Tu", "W", "Th", "F", "Sa"};
+     private static final String[] MONTHS = {"January","February","March","April","May","June","July","August","September","October","November","December"};
+     private static final int[] YEARS = {2017,2018,2019,2020,2021,2022,2023,2024,2025,2026,2027};
+     public int i = 1, j=0;
      
      private ArrayList<Button> allButtons = new ArrayList<Button>();
      //Button newButton = new Button("");
@@ -44,16 +41,19 @@ public class PivDisplayCalendar extends Container {
             Container title = new Container(new GridLayout(1,7));
             Container days = new Container(new GridLayout(6, 7));
             Container calendarTitleCopy = new Container(new GridLayout(1, 1));
-                                this.addComponent(calendarTitleCopy);
-                                this.addComponent(title);
-                                this.addComponent(days);
+                calendarTitleCopy.setUIID("CalendarTitleCopy");
+            this.addComponent(calendarTitleCopy);
+            this.addComponent(title);
+            this.addComponent(days);
     
                                
             Button prevMonth = new Button("<");
             Button nextMonth = new Button(">");
-            SpanLabel monthYear = new SpanLabel("Month"+"Year");
+            Label month = new Label(MONTHS[0]);
+            Label yearLabel = new Label( "2017");
             calendarTitle.add(BorderLayout.WEST, prevMonth);
-            calendarTitle.add(BorderLayout.CENTER, monthYear);
+            Container TitleLabel = FlowLayout.encloseIn(month,yearLabel);
+            calendarTitle.add(BorderLayout.CENTER, TitleLabel);
             calendarTitle.add(BorderLayout.EAST, nextMonth);
             calendarTitleCopy.add(calendarTitle);
             Button dayButton= new Button();
@@ -68,45 +68,56 @@ public class PivDisplayCalendar extends Container {
             for (int iter = 0; iter < DAYS.length; iter++) {
                 title.addComponent(createDayTitle(iter));
             }
-            for (int iter = 0; iter < length; iter++) {
+            for (int iter = 1; iter < length; iter++) {
                dayButton = new Button(""+iter);
                
                     dayButton.addActionListener(new ActionListener() {
-                        
                         @Override
                         public void actionPerformed(ActionEvent evt) {
                             Log.p("Action event triggered");
+                            Picker datePicker = new Picker();
+                            datePicker.setType(Display.PICKER_TYPE_DATE);
                             
-                            Button b1 = (Button)(evt.getActualComponent());
-                            Log.p( b1.getText() );
+                            //Button b1 = (Button)(evt.getActualComponent());
+                            //Log.p( b1.getText() );
                         }
                     });
                
                allButtons.add(dayButton);
-                
-                /*
-                Picker datePicker = new Picker();
-                datePicker.setText(""+iter);
-                */
-                //datePicker.setType(Display.PICKER_TYPE_DATE);
-               // buttons[iter] = new Button(""+iter);
                 days.addComponent(dayButton);
                 if (iter <= 7) {
                     dayButton.setNextFocusUp(year);
                 }
-                
-            }   
+            }    
             
-           
+            nextMonth.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
                 
+                month.setText(MONTHS[i]);
+                yearLabel.setText(""+YEARS[j]);
+                if((i<MONTHS.length-1)){
+                    i++;
+                }
+                else{
+                    i=0;
+                    j++;
+                }
+               
+                
+            }
+        });
+           
     }
-   
-    
+       
      protected Label createDayTitle(int day) {
-        String value = getUIManager().localize("Calendar." + DAYS[day], LABELS[day]);
-        Label dayh = new Label(value, "CalendarTitle");
+        String value = getUIManager().localize("Calendar" + DAYS[day], LABELS[day]);
+        Label dayh = new Label(value, "Label");
         dayh.setEndsWith3Points(false);
         dayh.setTickerEnabled(false);
         return dayh;
     }
+     
+     
+    
 }
